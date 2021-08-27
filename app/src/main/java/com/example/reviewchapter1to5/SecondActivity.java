cb786380e723c727f5e5bd6105517c4f6ea51902
Creater66 <1981190645@qq.com>
2021-08-27 16:36:07 +0800

    First commit

---
+++ app/src/main/java/com/example/reviewchapter1to5/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.reviewchapter1to5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
